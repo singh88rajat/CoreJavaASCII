ba21efb3ac4c01f18e52319cf225c0149832931a
Mukesh Kumar <singh88rajat@gmail.com>
2016-12-17 22:13:41 +0530

    CoreJavaASCIII

---
+++ src/MyFourthAssignment.java
@@ -0,0 +1,16 @@
+
+public class MyFourthAssignment {
+
+	 public static void main(String args[])
+	    {
+		
+	        String ch;
+	        int i;
+	        for(i=65; i<=90; i++)
+	        {
+	            ch = new Character((char)i).toString();
+	            System.out.println(i+ " -> " + ch + "\t");
+	        }
+			
+	    }
+}
